1cec7bc088d2c42ffdda4e159ed7b3e898266b36
TarlanSB <tarlanworkmail@gmail.com>
2023-07-04 21:59:04 +0300

    Рефакторинг кода
    - переименование переменной
    - удаление дублирования

--- MyFirstGame.java
+++ MyFirstGame.java
@@ -2,22 +2,19 @@ public class MyFirstGame {
     
     public static void main(String[] args) {
         int guessedNum = 8; 
-        int userNum = 33; 
+        int playerNum = 33; 
         int min = 1;
         int max = 100;
         
-        while (guessedNum != userNum) {
-            if (userNum > guessedNum) {
-                System.out.println("Число " + userNum + " больше того, что загадал компьютер");
-                max = userNum;
-                userNum = (min + max) / 2;
-            } else {                
-                System.out.println("Число " + userNum + " меньше того, что загадал компьютер");
-                if (userNum <= max) {
-                    min = userNum;
-                    userNum =  (max + min) / 2;
-                } 
+        while (guessedNum != playerNum) {
+            if (playerNum > guessedNum) {
+                System.out.println("Число " + playerNum + " больше того, что загадал компьютер");
+                max = playerNum;
+            } else {
+                System.out.println("Число " + playerNum + " меньше того, что загадал компьютер");
+                min = playerNum;
             }
+            playerNum = (min + max) / 2;
         }
 
         System.out.println("Вы победили!");
