7eadf1931448f8a6a632e1b8d81c2c0b2b4ab22e
TarlanSB <tarlanworkmail@gmail.com>
2023-07-05 23:47:59 +0300

    class Person created

---
+++ Lesson_2/Person.java
@@ -0,0 +1,28 @@
+public class Person{
+
+    char sex = 'm';
+    String name = "Peter";
+    double height = 1.75;
+    double weight = 75.5;
+    int age = 30;
+
+    public void move() {
+        System.out.println("идет");
+    }
+
+    public void sit() {
+        System.out.println("сидит");
+    }
+
+    public String run() {
+        return "бежит";
+    } 
+
+    public String speak() {
+        return "говорит";
+    }
+
+    public void study() {
+        System.out.println("учит Java");
+    } 
+}
