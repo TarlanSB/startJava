4bb112e251661bd33324f70bc4bf9b1f0591c116
TarlanSB <tarlanworkmail@gmail.com>
2023-07-10 01:59:42 +0300

    class GuessNumberTest is created

---
+++ Lesson_2/GuessNumberTest.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class GuessNumberTest {
+    
+    public static void main(String[] args) {
+        System.out.println("Введите имя (первый игрок)");
+        Scanner scanner = new Scanner(System.in);
+        Player player1 = new Player(scanner.next());
+
+        System.out.println("Введите имя (второй игрок)");
+        scanner = new Scanner(System.in);
+        Player player2 = new Player(scanner.next());
+
+        GuessNumber game = new GuessNumber(player1, player2);
+
+        while (true) {
+            game.startGame();
+
+            while (true) {
+                System.out.println("Хотите продолжить вычисления? [yes/no]: ");
+                
+                String carryOn = scanner.next();
+                carryOn = carryOn.toLowerCase();
+
+                if (carryOn.equals("yes")) {
+                    break;
+                } else if (carryOn.equals("no")) {
+                    return;
+                } else {
+                    continue;
+                }
+            }
+        }
+    }
+}
