fe0ba623fffb40c57e99d0f082923526e9f0983b
TarlanSB <tarlanworkmail@gmail.com>
2023-07-23 21:54:55 +0300

    Рефакторинг - переименование переменных - сообщение о ошибке знака перенесен в switch -удален метод output

--- src/com/startjava/lesson_2_3_4/calculator/Calculator.java
+++ src/com/startjava/lesson_2_3_4/calculator/Calculator.java
@@ -3,57 +3,54 @@ package com.startjava.lesson_2_3_4.calculator;
 public class Calculator {
 
     private String sign;
-    private int variables1;
-    private int variables2;
-    private double result;
+    private int firstNumber;
+    private int secondNumber;
+
+    public String getSign() {
+        return sign;
+    }
+
+    public int getFirstNumber() {
+        return firstNumber;
+    }
+
+    public int getSecondNumber() {
+        return secondNumber;
+    }
+
+    public Calculator() {
+    }
 
     public double calculate(String mathExpression) {
-        String[] varsOrSign = mathExpression.split(" ");
-        sign = varsOrSign[1];
-        variables1 = Integer.parseInt(varsOrSign[0]);
-        variables2 = Integer.parseInt(varsOrSign[2]);
+        String[] compositionOfExpression = mathExpression.split(" ");
+        sign = compositionOfExpression[1];
+        firstNumber = Integer.parseInt(compositionOfExpression[0]);
+        secondNumber = Integer.parseInt(compositionOfExpression[2]);
+        double result;
         switch (sign) {
             case "+":
-                result = variables1 + variables2;
+                result = firstNumber + secondNumber;
                 break;
             case "-":
-                result = variables1 - variables2;
+                result = firstNumber - secondNumber;
                 break;
             case "*":
-                result = variables1 * variables2;
+                result = firstNumber * secondNumber;
                 break;
             case "^":
-                result = Math.pow(variables1, variables2);
+                result = Math.pow(firstNumber, secondNumber);
                 break;
             case "%":
-                result = variables1 % variables2;
+                result = firstNumber % secondNumber;
                 break;
             case "/":
-                result = (double) variables1 / variables2;
+                result = (double) firstNumber / secondNumber;
                 break;
             default:
+                System.out.println("Ошибка: знак " + sign + " не поддерживается");
                 result = Double.MIN_VALUE;
                 break;
         }
         return result;
-    }
-
-    public void output() {
-        if (result != Double.MIN_VALUE) {
-            double copyResult = result;
-            int sumFractionalPart = 0;
-            String stringResult = String.format("%.0f", result);
-            for (int i = 0; i < 3; i++) {
-                copyResult *= 10;
-                sumFractionalPart += (int) (copyResult % 10);
-                if (sumFractionalPart > 0) {
-                    stringResult = String.format("%.3f", result);
-                    break;
-                }
-            }
-            System.out.println(variables1 + " " + sign + " " + variables2 + " = " + stringResult);
-        } else {
-            System.out.println("Ошибка: знак " + sign + " не поддерживается");
-        }
     }
 }
