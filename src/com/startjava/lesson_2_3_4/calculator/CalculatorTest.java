28ddd225ed4e267f5299326721e5b0d92f4dfeb8
TarlanSB <tarlanworkmail@gmail.com>
2023-07-23 21:56:49 +0300

    Рефакторинг - вывод результата происходит в этом классе - вместо вложенного цикла в CalculatorTest используйте if

--- src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
+++ src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
@@ -1,5 +1,6 @@
 package com.startjava.lesson_2_3_4.calculator;
 
+import java.text.NumberFormat;
 import java.util.Scanner;
 
 public class CalculatorTest {
@@ -7,19 +8,23 @@ public class CalculatorTest {
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
         Scanner scanner = new Scanner(System.in);
-        String answer = "";
-
-        while (!answer.equals("no")) {
-            System.out.print("Введите математическое выражение в формате: 2 ^ 10 ");
-            String mathExpression = scanner.nextLine();
-
-            calculator.calculate(mathExpression);
-            calculator.output();
-
-            do {
-                System.out.println("Хотите продолжить вычисления? [yes/no]: ");
-                answer = scanner.nextLine().toLowerCase();
-            } while (!answer.equals("yes") && !answer.equals("no"));
-        }
+        String answer = "yes";
+        NumberFormat nf = NumberFormat.getInstance();
+        do {
+            if (answer.equals("yes")) {
+                System.out.print("Введите математическое выражение в формате: 2 ^ 10 ");
+                String mathExpression = scanner.nextLine();
+                double result = calculator.calculate(mathExpression);
+                if (result != Double.MIN_VALUE) {
+                    System.out.println(calculator.getFirstNumber() + " " + calculator.getSign() + " "
+                            + calculator.getSecondNumber() + " = " + nf.format(result));
+                }
+            }
+            System.out.println("Хотите продолжить вычисления? [yes/no]: ");
+            answer = scanner.nextLine().toLowerCase();
+            if (answer.equals("no")) {
+                break;
+            }
+        } while (!answer.equals("no"));
     }
 }
