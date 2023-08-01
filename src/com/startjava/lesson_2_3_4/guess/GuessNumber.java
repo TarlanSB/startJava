627d71a6c742d465aaf1aaa550657df3ff9879ca
TarlanSB <tarlanworkmail@gmail.com>
2023-08-02 01:17:00 +0300

    Рефакторинг кода -декомпозиция кода внутри цикла -декомпозиция и создание методов compareNum, output, clean, input, namedNum

--- src/com/startjava/lesson_2_3_4/guess/GuessNumber.java
+++ src/com/startjava/lesson_2_3_4/guess/GuessNumber.java
@@ -6,6 +6,7 @@ import java.util.Scanner;
 public class GuessNumber {
     private final Player player1;
     private final Player player2;
+    private final Scanner scanner = new Scanner(System.in);
 
     public GuessNumber(String name1, String name2) {
         player1 = new Player(name1);
@@ -15,59 +16,65 @@ public class GuessNumber {
     public void start() {
         System.out.println("Игра началась! У каждого игрока по 10 попыток");
         int guessedNum = 1 + (int) (Math.random() * 100);
-        Scanner scanner = new Scanner(System.in);
-        boolean state1 = true;
 
-        while (state1) {
-            System.out.println(player1.getName() + " введите число");
-            player1.setNumber(scanner.nextInt());
-            player1.addNum(player1.indexStorage, player1.getNumber());
-            boolean state2 = compareNum(player1, player1.indexStorage, player1.getNumber(), guessedNum);
-            player1.indexStorage++;
-            if (!state2) {
+        while (player1.getAttempt() < 10) {
+            if (compareNum(player1, guessedNum)) {
                 break;
             }
-
-            System.out.println(player2.getName() + " введите число");
-            player2.setNumber(scanner.nextInt());
-            player2.addNum(player2.indexStorage, player2.getNumber());
-            state1 = compareNum(player2, player2.indexStorage, player2.getNumber(), guessedNum);
-            player2.indexStorage++;
-
-            if (player1.indexStorage > 8) {
-                output();
+            if (compareNum(player2, guessedNum)) {
                 break;
             }
         }
+        output(player1, player2);
+        clean(player1, player2);
     }
 
-    public void output() {
-        System.out.println("Названные числа игрока " + player1.getName() + ": "
-                + (Arrays.toString(player1.getStorage()).replaceAll("[\\[\\],]", ""))
-                + "\nназванные числа игрока " + player2.getName() + ": "
-                + (Arrays.toString(player2.getStorage())).replaceAll("[\\[\\],]", ""));
-    }
+    public boolean compareNum(Player player, int guessedNum) {
 
-    public boolean compareNum(Player player, int indexStorage, int playerNumber, int guessedNum) {
-        if (playerNumber == guessedNum) {
+        inputNum(player);
+        if (namedNum(player) == guessedNum) {
             System.out.println("Игрок " + player.getName() + " угадал " + guessedNum
-                    + " с " + (++indexStorage) + " попытки");
-            output();
-            return false;
-        } else if (playerNumber > guessedNum) {
-            System.out.println("Число " + playerNumber + " больше того, что загадал компьютер");
+                    + " с " + (player.getAttempt() + 1) + " попытки");
             return true;
+        }
+        if (namedNum(player) > guessedNum) {
+            System.out.println("Число " + namedNum(player)
+                    + " больше того, что загадал компьютер");
         } else {
-            System.out.println("Число " + playerNumber + " меньше того, что загадал компьютер");
-            return true;
+            System.out.println("Число " + namedNum(player)
+                    + " меньше того, что загадал компьютер");
+        }
+
+        player.setAttempt(player.getAttempt() + 1);
+        return false;
+    }
+
+    public void inputNum(Player player) {
+        System.out.println(player.getName() + " введите число");
+        player.addNum(scanner.nextInt());
+    }
+
+    public int namedNum(Player player) {
+        return player.getStorage()[player.getAttempt()];
+    }
+
+    public void output(Player... players) {
+        for (Player player : players) {
+            System.out.print("Названные числа игрока " + player.getName() + ":");
+            for (int i = 0; i < player.getStorage().length; i++) {
+                if (player.getStorage()[i] != 0) {
+                    System.out.print(" " + player.getStorage()[i]);
+                }
+            }
+            System.out.println();
         }
     }
 
-    public void clean() {
-        Arrays.fill(player1.getStorage(), 0, player1.indexStorage, 0);
-        Arrays.fill(player2.getStorage(), 0, player2.indexStorage, 0);
-        player1.indexStorage = 0;
-        player2.indexStorage = 0;
+    public void clean(Player... players) {
+        for (Player player : players) {
+            Arrays.fill(player.getStorage(), 0, player.getAttempt() + 1, 0);
+            player.setAttempt(0);
+        }
     }
 }
 
