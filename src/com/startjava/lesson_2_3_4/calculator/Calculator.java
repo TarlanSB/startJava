166e841599371b91dad91744acf7ab3dd8aafe2b
TarlanSB <tarlanworkmail@gmail.com>
2023-08-08 19:46:17 +0300

    На каждый вид ошибки выводит свое сообщение

--- src/com/startjava/lesson_2_3_4/calculator/Calculator.java
+++ src/com/startjava/lesson_2_3_4/calculator/Calculator.java
@@ -2,27 +2,46 @@ package com.startjava.lesson_2_3_4.calculator;
 
 public class Calculator {
 
-    public double calculate(String mathExpression) {
-        String[] elements = mathExpression.split(" ");
-        String sign = elements[1];
-        int firstNumber = Integer.parseInt(elements[0]);
-        int secondNumber = Integer.parseInt(elements[2]);
-        switch (sign) {
-            case "+":
-                return firstNumber + secondNumber;
-            case "-":
-                return firstNumber - secondNumber;
-            case "*":
-                return firstNumber * secondNumber;
-            case "^":
-                return Math.pow(firstNumber, secondNumber);
-            case "%":
-                return firstNumber % secondNumber;
-            case "/":
-                return (double) firstNumber / secondNumber;
-            default:
-                System.out.println("Ошибка: знак " + sign + " не поддерживается");
-                return Double.MIN_VALUE;
+    public static double calculate(String expression) {
+        String[] elements = expression.split(" ");
+        double result = 0;
+
+        try {
+            if (elements.length > 3) {
+                throw new ArrayIndexOutOfBoundsException("Длина выражение больше, чем требуется " +
+                        "- Математическое выражение должно быть в формате: \"2 ^ 10\"");
+            }
+            int firstNumber = Integer.parseInt(elements[0]);
+            String sign = elements[1];
+            String lastNum = String.valueOf(elements.length);
+            System.out.println(lastNum);
+            int secondNumber = Integer.parseInt(elements[2]);
+            boolean isCorrectNumbers = state(firstNumber, secondNumber);
+            if (!isCorrectNumbers) {
+                throw new NumberFormatException("Недопустимы ввод - числа должны быть от 1 до 100 включительно");
+            }
+
+            return result = switch (sign) {
+                case "+" -> firstNumber + secondNumber;
+                case "-" -> firstNumber - secondNumber;
+                case "*" -> firstNumber * secondNumber;
+                case "^" -> Math.pow(firstNumber, secondNumber);
+                case "%" -> firstNumber % secondNumber;
+                case "/" -> (double) firstNumber / secondNumber;
+                default -> {
+                    System.out.println("Ошибка: знак " + sign + " не поддерживается");
+                    yield Double.MIN_VALUE;
+                }
+            };
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("Математическое выражение должно быть в формате: 2 ^ 10");
+        } catch (NumberFormatException e) {
+            System.out.println("Недопустимы ввод - числа должны быть от 1 до 100 включительно");
         }
+        return result;
+    }
+
+    public static boolean state(int number1, int number2) {
+        return (number1 > 0 && number1 <= 100) && (number2 > 0 && number2 <= 100);
     }
 }
--- src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
+++ src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
@@ -8,25 +8,23 @@ public class CalculatorTest {
     static NumberFormat nf = NumberFormat.getInstance();
 
     public static void main(String[] args) {
-        Calculator calculator = new Calculator();
+        //Calculator calculator = new Calculator();
         Scanner scanner = new Scanner(System.in);
         String answer = "yes";
 
         do {
             if (answer.equals("yes")) {
                 System.out.print("Введите математическое выражение в формате: 2 ^ 10 ");
-                String mathExpression = scanner.nextLine();
-                output(calculator, mathExpression);
+                output(scanner.nextLine());
             }
             System.out.println("Хотите продолжить вычисления? [yes/no]: ");
             answer = scanner.nextLine().toLowerCase();
         } while (!answer.equals("no"));
     }
 
-    static void output(Calculator calculator, String mathExpression) {
-        double result = calculator.calculate(mathExpression);
-        if (result != Double.MIN_VALUE) {
-            System.out.println(mathExpression + " = " + nf.format(result));
-        }
+    static void output(String mathExpression) {
+
+        double result = Calculator.calculate(mathExpression);
+        System.out.println((result > Double.MIN_VALUE) ? mathExpression + " = " + nf.format(result) : "");
     }
 }
