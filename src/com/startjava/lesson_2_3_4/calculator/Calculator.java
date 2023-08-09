b0f0fa0c61265b56fe597eb46b65dba51d04842a
TarlanSB <tarlanworkmail@gmail.com>
2023-08-09 21:32:59 +0300

    Рефактоинг -изменение инциализации переменной result -использование метода checkNumbers -рефакторинг блока кода if (elements.length > 3) -удаление catch (ArrayIndexOutOfBoundsException e) -рефакторинг метода checkNumbers

--- src/com/startjava/lesson_2_3_4/calculator/Calculator.java
+++ src/com/startjava/lesson_2_3_4/calculator/Calculator.java
@@ -4,44 +4,44 @@ public class Calculator {
 
     public static double calculate(String expression) {
         String[] elements = expression.split(" ");
-        double result = 0;
+        double result = Double.MIN_VALUE;
 
         try {
             if (elements.length > 3) {
-                throw new ArrayIndexOutOfBoundsException();
+                System.out.println("Длина выражение больше, чем требуется - " +
+                        "математическое выражение должно быть в формате: \"2 ^ 10\"");
+                return Double.MIN_VALUE;
             }
             int firstNumber = Integer.parseInt(elements[0]);
             String sign = elements[1];
             int secondNumber = Integer.parseInt(elements[2]);
-            checkNumbers(firstNumber, secondNumber);
+            boolean state = checkNumbers(firstNumber, secondNumber);
 
-            return result = switch (sign) {
-                case "+" -> firstNumber + secondNumber;
-                case "-" -> firstNumber - secondNumber;
-                case "*" -> firstNumber * secondNumber;
-                case "^" -> Math.pow(firstNumber, secondNumber);
-                case "%" -> firstNumber % secondNumber;
-                case "/" -> (double) firstNumber / secondNumber;
-                default -> {
-                    System.out.println("Ошибка: знак " + sign + " не поддерживается");
-                    yield Double.MIN_VALUE;
-                }
-            };
-        } catch (ArrayIndexOutOfBoundsException e) {
-            System.out.println("Длина выражение больше, чем требуется - " +
-                    "математическое выражение должно быть в формате: \"2 ^ 10\"");
+            if (state) {
+                return result = switch (sign) {
+                    case "+" -> firstNumber + secondNumber;
+                    case "-" -> firstNumber - secondNumber;
+                    case "*" -> firstNumber * secondNumber;
+                    case "^" -> Math.pow(firstNumber, secondNumber);
+                    case "%" -> firstNumber % secondNumber;
+                    case "/" -> (double) firstNumber / secondNumber;
+                    default -> {
+                        System.out.println("Ошибка: знак " + sign + " не поддерживается");
+                        yield Double.MIN_VALUE;
+                    }
+                };
+            }
         } catch (NumberFormatException e) {
-            System.out.println("Недопустимы ввод - числа должны быть от 1 до 100 включительно");
+            System.out.println("Недопустимы ввод - числа должны быть целыми и положительными");
         }
         return result;
     }
 
     public static boolean checkNumbers(int number1, int number2) {
-        if ((number1 > 0 && number1 <= 100) && (number2 > 0 && number2 <= 100)) {
+        if ((number1 > 0) && (number2 > 0)) {
             return true;
-        } else {
-            throw new NumberFormatException();
         }
+        throw new NumberFormatException();
     }
 }
 
