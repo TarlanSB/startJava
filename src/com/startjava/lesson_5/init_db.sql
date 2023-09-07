d317399f51ff59f5c1b774a7f3a8e5c4a73b859a
TarlanSB <tarlanworkmail@gmail.com>
2023-09-07 21:28:19 +0300

    Файл init_db.sql  is created

---
+++ src/com/startjava/lesson_5/init_db.sql
@@ -0,0 +1,11 @@
+INSERT INTO jaegers(model_name, mark, height, weight, status, origin, launch, kaiju_kill)
+VALUES ('Cherno Alpha', 'Mark-1', 85.34, 2.412, 'Destroyed', 'Russia', '2015-05-01', 6),
+       ('Diablo Intercept', 'Mark-2', 75.25, 2.22, 'Destroyed', 'Chile', '2016-01-01', 4),
+       ('Gipsy Danger', 'Mark-3', 79.25, 1.98, 'Destroyed', 'United States of America', '2017-07-10', 9),
+       ('Crimson Typhoon', 'Mark-4', 76.2, 1.722, 'Destroyed', 'China', '2018-08-22', 7),
+       ('Striker Eureka', 'Mark-5', 76.2, 1.85, 'Destroyed', 'Australia', '2019-11-02', 11),
+       ('Bracer Phoenix', 'Mark-5', 70.91, 2.128, 'Destroyed', 'Unknown', '2025-11-01', 7),
+       ('Guardian Bravo', 'Mark-6', 73.21, 1.975, 'Active', 'Unknown', '2033-12-15', 1),
+       ('Titan Redeemer', 'Mark-6', 75.931, 2.31, 'Destroyed', 'Unknown', '2033-10-5', 3),
+       ('Gipsy Avenger', 'Mark-6', 81.77, 2.004, 'Destroyed', 'Unknown', '2034-01-12', 1),
+       ('Saber Athena', 'Mark-7', 76.91, 1.628, 'Destroyed', 'Unknown', '2033-12-20', 1);
